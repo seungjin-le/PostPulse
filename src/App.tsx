f6e7b3e001da9f6fd12a079d56f3a529bef31fbb
Seungjin Lee <dltmdwls154@gmail.com>
2023-06-23 23:07:01 +0900

    tailwindcss 셋팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
-import './styles/tailwind.css'
 
 function App() {
-  return <div className='App'></div>
+  return <div className='App bg-[red] text-white p-4'>123123123123123</div>
 }
 
 export default App
